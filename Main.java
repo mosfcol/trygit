0b221885521cf83413648d3e419fa7badfa59178
Shaofeng Mo <mosfcol@gmail.com>
2015-04-16 21:08:10 -0700

    add sayhi()

--- Main.java
+++ Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hey watsup!!");
     }
+
+    public static void sayhi() {
+        System.out.println("say hi!");
+    }
 }
